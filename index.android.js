63d9f4e9d6098e28d4e16674dca53d304bbd330a
ninjaPixel <matthew@ninjaPixel.eu>
2017-03-28 23:30:13 +0100

    Add Home screen for iOS and Android

---
+++ index.android.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import CityList from './app/components/CityList';
+
+export default class TripRevisor extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <CityList/>
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('TripRevisor', () => TripRevisor);
