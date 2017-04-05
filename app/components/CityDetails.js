1e16fbd89074af4b0516e60c00998e8a5f918faf
ninjaPixel <matthew@ninjaPixel.eu>
2017-04-05 20:57:21 +0100

    Connect to meteor server

--- app/components/CityDetails.js
+++ app/components/CityDetails.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import Meteor from 'react-native-meteor';
 import { ScrollView, Text } from 'react-native';
 
-const CityDetails = props => (
-  <ScrollView>
-    <Text>Hello, {props.name}!</Text>
-  </ScrollView>
-);
+
+class CityDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      error: '',
+    };
+  }
+
+  componentDidMount() {
+    const params = { name: this.props.navigation.state.params.name };
+    Meteor.call('cities.findOne', params, (err, cities) => {
+      if (err) {
+        console.log(err);
+        this.setState({ error: err.reason });
+
+      } else {
+        console.log('cities', cities);
+        this.setState({ error: '' });
+
+      }
+      this.setState({ loading: false });
+    });
+  }
+
+  render() {
+    const isLoading = this.state.loading;
+    const error = this.state.error;
+    return (
+      <ScrollView>
+        { isLoading ? (
+          <Text>Loading {this.props.navigation.state.params.name} details...</Text>
+        ) : (
+          <Text>Hello, {this.props.navigation.state.params.name}!</Text>
+        )}
+
+        {error ? (
+          <Text>{error}</Text>
+        ): (null)}
+      </ScrollView>
+    );
+  }
+}
 
 export default CityDetails;
--- app/components/Navigator.js
+++ app/components/Navigator.js
@@ -1,13 +1,28 @@
 import { StackNavigator } from 'react-navigation';
+import Meteor from 'react-native-meteor';
 import HomeScreen from './HomeScreen';
 import CityDetails from './CityDetails';
 
+Meteor.connect('ws://192.168.1.66:3000/websocket');
 
 const Navigator = StackNavigator({
-  Home: { screen: HomeScreen },
-  City: { screen: CityDetails },
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      header: {
+        visible: false,
+      },
+    },
+  },
+  City: {
+    screen: CityDetails,
+    navigationOptions: {
+      title: ({ state }) => state.params.name,
+    },
+  },
 }, {
-  initialRouteName: 'Home' },
-);
+  initialRouteName: 'Home',
+  headerMode: 'screen',
+});
 
 export default Navigator;
